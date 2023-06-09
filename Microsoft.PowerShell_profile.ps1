e7b049773c179fe548f274e3e637a04170d55cf6
Alexander Thiem <thiemalexs@gmail.com>
2023-06-09 16:16:08 -0700

    update VSCode and PS settings

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -12,3 +12,17 @@ function Update_Powershell {winget install --id Microsoft.Powershell --source wi
 New-Alias -Name upsh -Value Update_Powershell
 
 $PSStyle.FileInfo.Directory = "`e[95m"
+
+# first word in PS is green
+Set-PSReadLineOption -Colors @{ Command = 'Green' }
+
+function Go_Back_Dir {cd ..}
+New-Alias -Name cc -Value Go_Back_Dir
+
+#function prompt {
+#   $currentDirectory = Get-Location
+#   "`r`n`e[34m$currentDirectory`r`n`e[33m❯ "
+#}
+
+Invoke-Expression (&starship init powershell)
+
